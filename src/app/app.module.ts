5d25b0c7edc8b1d484ef14519b51cb4beb05bfbc
Antonio Tomas <antoniotd87@gmail.com>
2021-01-21 19:50:08 -0600

    PracticaFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarraComponent } from './barra/barra.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
